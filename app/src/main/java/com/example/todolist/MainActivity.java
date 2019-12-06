2c7a350832338dda371bb35ba723cc333213b98c
yeril <dj2611@wp.pl>
2019-12-06 23:14:53 +0100

    ekran startowy - statyczna lista, button

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.todolist;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add(new String("Egzaminy"));
+        stringArrayList.add(new String("Zakupy"));
+        stringArrayList.add(new String("Praca"));
+        stringArrayList.add(new String("SMS"));
+
+        ListView listView = findViewById(R.id.List);
+
+        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stringArrayList);
+        listView.setAdapter(arrayAdapter);
     }
 }
